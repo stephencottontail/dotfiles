28484d22705101b09d2197ac07e5e1c1b801f664
Stephen Dickinson <stephencottontail@me.com>
2023-01-30 13:48:17 -0700

    Copy all the files

--- install.sh
+++ install.sh
@@ -2,4 +2,6 @@
 
 # copy files from /workspaces/.codespaces/.persistedshare/dotfiles
 cp -r /workspaces/.codespaces/.persistedshare/dotfiles/.config/vim $HOME/.config/vim
+cp /workspaces/.codespaces/.persistedshare/dotfiles/.bashrc $HOME/.bashrc
+cp /workspaces/.codespaces/.persistedshare/dotfiles/.gitmodules $HOME/.gitmodules
 
